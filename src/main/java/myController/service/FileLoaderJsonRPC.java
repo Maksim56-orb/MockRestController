6fdef391da3dc26e248a1375f5a553627cffbcdc
Maksim <m.tumaykin@mail.ru>
2025-12-13 17:02:31 +0500

    add Filebit

--- src/main/java/myController/service/FileLoaderJsonRPC.java
+++ src/main/java/myController/service/FileLoaderJsonRPC.java
@@ -35,7 +35,6 @@ public void init() {
             getFileName(getCollection), getFileContent(getCollection),
             getFileName(sendEvent), getFileContent(sendEvent)
     );
-   // log.info("Files content: {}", filesContent);
 }
 
 private String getFileName(Resource resource) {
